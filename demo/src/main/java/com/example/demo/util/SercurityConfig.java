d58984fe828fb10cf665cc2968b14c9297716670
mhheoSunyoutech <mhheo@sunyoutech.com>
2024-12-23 17:08:08 +0900

    jwt 리팩토링

--- demo/src/main/java/com/example/demo/util/SercurityConfig.java
+++ demo/src/main/java/com/example/demo/util/SercurityConfig.java
@@ -1,13 +1,10 @@
 package com.example.demo.util;
 
 import com.example.demo.filter.JwtAuthenticationFilter;
-import jakarta.servlet.Filter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.DefaultSecurityFilterChain;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -21,6 +18,7 @@ public class SercurityConfig {
         this.jwtAuthenticationFilter = jwtAuthenticationFilter;
     }
 
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
@@ -28,7 +26,7 @@ public class SercurityConfig {
                 .authorizeHttpRequests(auth->auth
                         .requestMatchers("/api/auth/login").permitAll()
                         .anyRequest().authenticated()
-                ).addFilterBefore((Filter) jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+                ).addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
